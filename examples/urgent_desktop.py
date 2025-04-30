7fd580cb22b8422da5b681dd4cdea1466a0571ea
Davide Manini <davide@manini.cloud>
2025-04-30 20:58:27 +0300

    examples: add urgent_desktop.py, a script moving to the urgent workspace

---
+++ examples/urgent_desktop.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import i3ipc
+from argparse import ArgumentParser
+
+
+
+
+def switch_to_workspace(connection, e):
+#    print(e.ipc_data["current"]["num"])
+    connection.command("workspace "+str(e.ipc_data["current"]["num"]))
+
+
+if __name__ == '__main__':
+    ArgumentParser(prog="urgent_desktop.py",
+                   description="""
+                   Automatically switch to a workspace containing a window
+                   that changed its status to urgent.
+                   """).parse_args()
+    
+    i3 = i3ipc.Connection()
+    i3.on(i3ipc.Event.WORKSPACE_URGENT, switch_to_workspace)
+    i3.main()
